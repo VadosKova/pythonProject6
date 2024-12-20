1b83aa95d8392ce96a8e98449cccd0866134faa1
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2024-12-20 10:48:40 +0200

    Created class

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ from tkinter import *
 from tkinter import ttk, messagebox
 import winreg
 
+class Registry:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Basic Registry")
+        self.root.geometry("600x400")
